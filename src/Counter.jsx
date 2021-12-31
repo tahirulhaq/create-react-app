4d7dacf40bf26c809fcebacdfb46a94804d14d9d
tahirulhaq <tahirul.haq@yahoo.com>
2021-12-31 14:43:33 +0500

    Counter.jsx

---
+++ src/Counter.jsx
@@ -0,0 +1,24 @@
+class Counter extends React.Component {
+  state = {
+    value: 0
+  };
+handleIncrement= () => {
+  this.setState(state => {
+     value: state.value + 1 
+  });
+};
+handleDecrement= () => {
+  this.setState(state => {
+     value: state.value - 1 
+  });
+};
+render() {
+    return (
+      <div>
+        <h2>{this.state.value}</h2>
+        <button onClick={this.handleDecrement}>Decrement</button>
+        <button onClick={this.handleIncrement}>Increment</button>
+      </div>
+    );
+}
+};
